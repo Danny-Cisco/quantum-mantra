74ff9a5189414c2f3531c57cc9ff416bcd48c62a
Danny Cisco <gumballelectronics@gmail.com>
2024-04-03 23:20:05 +1100

    basic layout

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -21,36 +21,22 @@
 		<!-- App Bar -->
 		<AppBar>
 			<svelte:fragment slot="lead">
-				<strong class="text-xl uppercase">Skeleton</strong>
+				<strong class="text-xl uppercase">Quantum Mantra</strong>
 			</svelte:fragment>
 			<svelte:fragment slot="trail">
 				<a
 					class="btn btn-sm variant-ghost-surface"
-					href="https://discord.gg/EXqV7W8MtY"
+					href="https://dannycisco.com"
 					target="_blank"
 					rel="noreferrer"
 				>
-					Discord
-				</a>
-				<a
-					class="btn btn-sm variant-ghost-surface"
-					href="https://twitter.com/SkeletonUI"
-					target="_blank"
-					rel="noreferrer"
-				>
-					Twitter
-				</a>
-				<a
-					class="btn btn-sm variant-ghost-surface"
-					href="https://github.com/skeletonlabs/skeleton"
-					target="_blank"
-					rel="noreferrer"
-				>
-					GitHub
+					by Danny Cisco
 				</a>
 			</svelte:fragment>
 		</AppBar>
 	</svelte:fragment>
 	<!-- Page Route Content -->
-	<slot />
+	<div class="flex flex-col items-center">
+		<slot />
+	</div>
 </AppShell>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,10 +1,1 @@
-<script>
-	let title = 'Title';
-	let paragraph = 'paragraph';
-</script>
-
-<div class="prose">
-	<h1>{@html title}</h1>
-	<p>{@html paragraph}</p>
-</div>
-ekjsgvskjvbsk
+<div class="text-xl text-bold">Quantum Mantra</div>
